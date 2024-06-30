c1aaa20f2287db1c0ab0c6c26c6f3e28a74574ad
riccardo-morelli <riccardo.morelli@edu.unife.it>
2024-06-30 18:38:16 +0200

    started implementing unit testing for snake

--- tests/test_unit_snake.py
+++ tests/test_unit_snake.py
@@ -1,8 +1,11 @@
 import pytest
-
+from game import snake
 class TestSnake:
-    def test_snake(self):
-        assert 1 == 1
-
-    def test_snake2(self):
-        assert 1 == 1
+    @pytest.mark.parametrize("key, current_direction, expected",
+                             [("KEY_UP", "DOWN", "UP"),
+                                ("KEY_DOWN", "RIGHT", "DOWN"),
+                                ("KEY_LEFT", "UP", "LEFT"),
+                                ("KEY_DOWN", "DOWN", "DOWN")])
+    def test_calcule_direction(self, key, current_direction, expected):
+        snake = snake.Snake()
+        assert snake.calcule_direction(current_direction, key) == expected
