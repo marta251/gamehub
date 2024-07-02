c754f553c9f32c9a3b57a04069a6804fa90a83e0
Marta Malagutti <marta.malagutti6133@gmail.com>
2024-07-02 14:17:55 +0200

    completed Game Of Life (except minor details)

--- gamehub/game-of-life.py
+++ gamehub/game-of-life.py
@@ -1,9 +1,8 @@
 import curses
 from curses import wrapper
 import random
-import time
 
-def draw_board(stdscr, rows, cols):
+def draw_board(stdscr, rows, cols) -> None:
     for i in range(rows):
         for j in range(cols):
             stdscr.addstr(i, j, ' ')
@@ -16,23 +15,50 @@ def draw_board(stdscr, rows, cols):
 
     stdscr.refresh()
 
-def main(stdscr):
+
+# TODO: Check the validity of the ranges for y and x
+
+def count_live_neighbors(stdscr, y, x) -> int:
+    count = 0
+    if stdscr.inch(y-1, x) == '0':
+        count += 1
+    if stdscr.inch(y, x-1) == '0':
+        count += 1
+    if stdscr.inch(y, x+1) == '0':
+        count += 1
+    if stdscr.inch(y+1, x) == '0':
+        count += 1
+
+    return count
+
+def update_board(stdscr, rows, cols) -> None:
+    for y in range(rows):
+        for x in range(cols):
+            live_neighbors = count_live_neighbors(stdscr, y, x)
+
+            if stdscr.inch(y, x) == '0':
+                if live_neighbors < 2 or live_neighbors > 3:
+                    stdscr.addstr(y, x, ' ') # Die because of underpopulation
+                elif live_neighbors == 3 or live_neighbors == 2:
+                    stdscr.addstr(y, x, '0') # Survive
+            else:
+                if live_neighbors == 3:
+                    stdscr.addstr(y, x, '0') # Reproduce
+    stdscr.refresh()
+
+def main(stdscr) -> None:
     curses.curs_set(0)  # Hide cursor
-    stdscr.nodelay(True)  # Non-blocking input
 
     (ROWS, COLS) = (curses.LINES - 1, curses.COLS - 1)
-    key_pressed = None
 
-    while key_pressed != 'q':
+    while True:
         stdscr.clear()
         draw_board(stdscr, ROWS, COLS)
         stdscr.refresh()
-
-        try:
-            key_pressed = stdscr.getkey()
-        except:
-            key_pressed = None
         
-        time.sleep(2)
+        if stdscr.getkey() == 'q': # Exit the game when 'q' is pressed
+            break
+        else:
+            update_board(stdscr, ROWS, COLS)
 
 wrapper(main)
