4e0de3e720770911ee415bd2f3872be5004015a0
riccardo-morelli <riccardo.morelli@edu.unife.it>
2024-07-09 12:36:25 +0200

    added function to check fen validity

--- gamehub/chess/chess_board.py
+++ gamehub/chess/chess_board.py
@@ -1,17 +1,18 @@
 from gamehub.chess.chess_piece import ChessPiece
 class ChessBoard:
 
-    #TODO: Check validity of FEN string
-
-    # Generates a chess board from a FEN string
+    # Generates a chess board from a FEN string or from a matrix and other parameters
     def __init__(self, fen=None, matrix=None, playerToMove=None, castlingRights=None, enPassant=None, halfMoveCounter=None, fullMoveCounter=None) -> None:
         if fen is not None:
-            self.matrix = self.convert_fen_to_matrix(fen)
-            self.playerToMove = fen.split(" ")[1]
-            self.castlingRights = fen.split(" ")[2]
-            self.enPassant = fen.split(" ")[3]
-            self.halfMoveCounter = int(fen.split(" ")[4])
-            self.fullMoveCounter = int(fen.split(" ")[5])
+            if not self.check_fen_validity(fen):
+                raise ValueError("Invalid FEN string")
+            fen_split = fen.split(" ")
+            self.matrix = self.convert_fen_to_matrix(fen_split[0])
+            self.playerToMove = fen_split[1]
+            self.castlingRights = fen_split[2]
+            self.enPassant = fen_split[3]
+            self.halfMoveCounter = int(fen_split[4])
+            self.fullMoveCounter = int(fen_split[5])
         elif matrix is not None and playerToMove is not None and castlingRights is not None and enPassant is not None and halfMoveCounter is not None and fullMoveCounter is not None:
             self.matrix = matrix
             self.playerToMove = playerToMove
@@ -21,6 +22,22 @@ class ChessBoard:
             self.fullMoveCounter = fullMoveCounter
         else:
             raise ValueError("Invalid arguments")
+        
+    def check_fen_validity(self, fen: str) -> bool:
+        fen_split = fen.split(" ")
+        if len(fen_split) != 6:
+            return False
+        if len(fen_split[0].split("/")) != 8:
+            return False
+        if fen_split[1] != "w" and fen_split[1] != "b":
+            return False
+        if len(fen_split[2]) > 4 or not all([c in "KQkq" for c in fen_split[2]]):
+            return False
+        if fen_split[3] != "-" and (len(fen_split[3]) != 2 or not fen_split[3][0].isalpha() or not fen_split[3][1].isdigit()):
+            return False
+        if not fen_split[4].isdigit() or not fen_split[5].isdigit():
+            return False
+        return True
 
     # Converts a FEN string to a 2D matrix
     def convert_fen_to_matrix(self, fen: str) -> list[list[ChessPiece]]:
