f3401debe85ae75389f37f1e66ac260c244913d4
Marta Malagutti <marta.malagutti6133@gmail.com>
2024-07-11 15:48:28 +0200

    two unnecessary assignments deleted in wordle.py

--- gamehub/wordle.py
+++ gamehub/wordle.py
@@ -8,6 +8,19 @@ class Wordle:
     def __init__(self):
         pass
 
+    def draw_initial_board(self, stdscr : object, guessed_word : str, guesses : int, alphabet : list) -> None:
+        stdscr.clear()
+        stdscr.addstr(1, 30, "Enter a five letter word, press Enter to confirm or ESC to exit.")
+        stdscr.addstr(2, 30, "Inserted word: " + "".join(guessed_word))
+        stdscr.addstr(3, 30, "Until now: " + "".join(guessed_word))
+        stdscr.addstr(4, 30, "Guesses left: " + str(guesses))
+        stdscr.addstr(6, 30, "Available letters: " + "".join(alphabet))
+        stdscr.refresh()
+
+    def draw_inserted_word(self, stdscr : object, void_guessed_word : list) -> None:
+        stdscr.addstr(2, 30, "Inserted word: " + "".join(void_guessed_word))
+        stdscr.refresh()
+
     def draw_after_update(self, stdscr : object, guessed_word : list, guesses : int, alphabet : list) -> None:
         stdscr.addstr(3, 30, "Until now: " + "".join(guessed_word))
         stdscr.addstr(4, 30, "Guesses left: " + str(guesses))
@@ -36,6 +49,11 @@ class Wordle:
         stdscr.addstr(5, 30, "                                                         ")
         stdscr.refresh()
 
+    def generate_list_of_words(self) -> list:
+        with open('/home/gamehub/files/words.txt', 'r') as file:
+            words = file.read().splitlines()
+        return words
+
     def generate_updated_guess(self, new_guessed : str, to_guess : str, alphabet : list, old_guessed : str) -> tuple:
         updated_guess = ["_" for _ in range(5)]
         for i in range(5): # For every character in the guessed word check if it is in the target word
@@ -53,36 +71,13 @@ class Wordle:
 
         return updated_guess, alphabet
 
-    def draw_initial_board(self, stdscr : object, guessed_word : str, guesses : int, alphabet : list) -> None:
-        stdscr.clear()
-        stdscr.addstr(1, 30, "Enter a five letter word, press Enter to confirm or ESC to exit.")
-        stdscr.addstr(2, 30, "Inserted word: " + "".join(guessed_word))
-        stdscr.addstr(3, 30, "Until now: " + "".join(guessed_word))
-        stdscr.addstr(4, 30, "Guesses left: " + str(guesses))
-        stdscr.addstr(6, 30, "Available letters: " + "".join(alphabet))
-        stdscr.refresh()
-
-    def draw_inserted_word(self, stdscr : object, void_guessed_word : list) -> None:
-        stdscr.addstr(2, 30, "Inserted word: " + "".join(void_guessed_word))
-        stdscr.refresh()
-
-    def generate_list_of_words(self) -> list:
-        with open('/home/gamehub/files/words.txt', 'r') as file:
-            words = file.read().splitlines()
-        return words
-
-    def check_terminal_size(self, min_lines : int, min_cols : int, win : object) -> bool:
+    def check_terminal_size(self, min_lines : int, min_cols : int, stdscr : object) -> bool:
         if curses.COLS < min_cols or curses.LINES < min_lines:
-            win.addstr(0, 0, "Resize the terminal (" + str(min_cols) + "x" + str(min_lines) + ")", curses.A_BOLD)
-            win.refresh()
+            stdscr.addstr(0, 0, "Resize the terminal (" + str(min_cols) + "x" + str(min_lines) + ")", curses.A_BOLD)
+            stdscr.refresh()
             time.sleep(2)
             return False
         return True
-    
-    def reinitialize_after_single_iteration(self) -> tuple:
-        characters = 0 # Number of characters inserted by the player
-        void_guessed_word = ["_" for _ in range(5)] # The space where the player inserts a new guess
-        return characters, void_guessed_word
     
     def initialize_game(self, stdscr : object) -> tuple:
         curses.curs_set(0)
@@ -93,6 +88,7 @@ class Wordle:
         alphabet = list(string.ascii_lowercase)
         game_stopped = False # Flag to stop the game (when the player wins or loses)
         self.draw_initial_board(stdscr, guessed_word, guesses, alphabet)
+
         return meaningful_words, word_to_guess, guessed_word, guesses, alphabet, game_stopped
 
     def gameloop(self, stdscr : object) -> None:
@@ -102,7 +98,8 @@ class Wordle:
         meaningful_words, word_to_guess, guessed_word, guesses, alphabet, game_stopped = self.initialize_game(stdscr)
 
         while not game_stopped:
-            characters, void_guessed_word = self.reinitialize_after_single_iteration()
+            characters = 0 # Number of characters inserted by the player
+            void_guessed_word = ["_" for _ in range(5)] # The space where the player inserts a new guess
             self.draw_inserted_word(stdscr, void_guessed_word)
 
             while characters <= 5:
@@ -126,7 +123,6 @@ class Wordle:
             void_guessed_word = ''.join(void_guessed_word) # Check if the guessed word is meaningful
             if void_guessed_word not in meaningful_words:
                 self.draw_after_invalid_input(stdscr)
-                characters, void_guessed_word = self.reinitialize_after_single_iteration()
             else:
                 guesses -= 1
                 guessed_word, alphabet = self.generate_updated_guess(void_guessed_word, word_to_guess, alphabet, guessed_word)
