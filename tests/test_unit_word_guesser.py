2f5a22e9ca7f2ce83c9f8f31fb7b93e1707090ea
Marta Malagutti <marta.malagutti6133@gmail.com>
2024-07-14 17:59:03 +0200

    added a gameloop test for Word Guesser when the user looses

--- tests/test_unit_word_guesser.py
+++ tests/test_unit_word_guesser.py
@@ -25,13 +25,17 @@ class TestWordGuesser:
         w = WordGuesser()
         assert w.generate_updated_guess(new_guessed, to_guess, alphabet, old_guessed) == expected
 
-    # With the following test we are going to test the gameloop when the user just wins immediately
-    def test_gameloop(self, monkeypatch) -> None:
+    # With the following method we are going to test the gameloop
+    # The user inserts the following words: apple (not the target word but a valid one),
+    # aboud (an invalid word) and think (the target word)
+    def test_gameloop_win(self, monkeypatch) -> None:
         def mock_inizialize_game(*args):
             return ["think", "apple", "about"], "think", ["_", "_", "_", "_", "_"], 6, list(string.ascii_lowercase), False
         
         def input_factory():
-            inputs = ["t","h","i","n","k","\n"]
+            inputs = ["a","p","p","l","e","\n",
+                      "a","b","o","u","d","\n",
+                      "t","h","u","\x7f","i","n","k","\n"]
             for i in inputs:
                 yield i
 
@@ -52,3 +56,36 @@ class TestWordGuesser:
         w = WordGuesser()
         w.gameloop(None)
         assert w.won is True
+
+    # With the following method we are going to test the gameloop when the user looses
+    def test_gameloop_loose(self, monkeypatch) -> None:
+        def mock_inizialize_game(*args):
+            return ["think", "apple", "about", "shark", "beach", "chair"], "table", ["_", "_", "_", "_", "_"], 6, list(string.ascii_lowercase), False
+        
+        def input_factory():
+            inputs = ["t","h","i","n","k","\n",
+                      "a","p","p","l","e","\n",
+                      "a","b","o","u","t","\n",
+                      "s","h","a","r","k","\n",
+                      "b","e","a","c","h","\n",
+                      "c","h","a","i","r","\n"]
+            for i in inputs:
+                yield i
+
+        input_gen = input_factory()
+
+        def get_next_input(*args):
+            return next(input_gen)
+        
+        monkeypatch.setattr(WordGuesser, "check_terminal_size", lambda *args: True)
+        monkeypatch.setattr(WordGuesser, "initialize_game", mock_inizialize_game)
+        monkeypatch.setattr(WordGuesser, "draw_inserted_word", lambda *args: None)
+        monkeypatch.setattr(WordGuesser, "get_key", get_next_input)
+        monkeypatch.setattr(WordGuesser, "draw_after_invalid_input", lambda *args: None)
+        monkeypatch.setattr(WordGuesser, "draw_after_update", lambda *args: None)
+        monkeypatch.setattr(WordGuesser, "draw_winning_message", lambda *args: None)
+        monkeypatch.setattr(WordGuesser, "draw_losing_message", lambda *args: None)
+
+        w = WordGuesser()
+        w.gameloop(None)
+        assert w.won is False
