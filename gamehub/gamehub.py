506a84273c8f753ae191b89f694bf847de1f48aa
riccardo-morelli <riccardo.morelli@edu.unife.it>
2024-07-16 18:25:52 +0200

    added spacing in help strings (gamehub)

--- gamehub/gamehub.py
+++ gamehub/gamehub.py
@@ -17,43 +17,43 @@ class GameHub:
     def setup_parsers(self) -> argparse.Namespace:
         parser = argparse.ArgumentParser(
             prog="gamehub",
-            description="GameHub is a game launcher for the terminal.")
-        subparsers = parser.add_subparsers(dest="game", help="The game to be played.")
+            description="GameHub is a game launcher for the terminal.\n")
+        subparsers = parser.add_subparsers(dest="game", help="The game to be played.\n")
 
         # Subparser for snake game
-        snake_parser = subparsers.add_parser("snake", help="Play Snake, a game where you control a snake that moves around a bordered area, collecting food pellets. Each time the snake eats a pellet, it grows longer. The challenge is to avoid colliding with the snake's own body or the borders of the play area. ")
+        snake_parser = subparsers.add_parser("snake", help="Play Snake, a game where you control a snake that moves around a bordered area, collecting food pellets. Each time the snake eats a pellet, it grows longer. The challenge is to avoid colliding with the snake's own body or the borders of the play area.\n")
         snake_parser.add_argument("--difficulty",
                                   type=str,
                                   default="Medium",
                                   choices=["Easy","Medium","Hard"],
-                                  help="The difficulty level of the game.")
+                                  help="The difficulty level of the game.\n")
 
         # Subparser for Game of Life
-        game_of_life_parser = subparsers.add_parser("game_of_life", help="Play Game of Life, a cellular automaton where you can observe the evolution of cells based on simple rules. It is a zero-player game, meaning that its evolution is determined by only its initial state, requiring no further input. ")
+        game_of_life_parser = subparsers.add_parser("game_of_life", help="Play Game of Life, a cellular automaton where you can observe the evolution of cells based on simple rules. It is a zero-player game, meaning that its evolution is determined by only its initial state, requiring no further input.\n")
         game_of_life_parser.add_argument("--mode",
                                          type=str,
                                          default="Automatic",
                                          choices=["Manual","Automatic"],
-                                         help="Manual: press any key to update; Automatic: update happens periodically.")
+                                         help="Manual: press any key to update; Automatic: update happens periodically.\n")
         game_of_life_parser.add_argument("--speed",
                                          type=int,
                                          default=100,
-                                         help="Choose the speed of the simulation in ms (useful just for Automatic mode).")
+                                         help="Choose the speed of the simulation in ms (useful just for Automatic mode).\n")
         game_of_life_parser.add_argument("--density",
                                          type=int,
                                          default=30,
-                                         help="The initial density of the cells in the grid.")
+                                         help="The initial density of the cells in the grid.\n")
 
         # Subparser for Word Guesser
-        subparsers.add_parser("word_guesser", help="Play Word Guesser, a game where You have to guess a random 5-letter English word. After each attempt the letters that are not present in the target word are removed from the available letters list and the ones that are present are shown on the terminal. You have six attempts to guess the word.")
+        subparsers.add_parser("word_guesser", help="Play Word Guesser, a game where You have to guess a random 5-letter English word. After each attempt the letters that are not present in the target word are removed from the available letters list and the ones that are present are shown on the terminal. You have six attempts to guess the word.\n")
         
         # Subparser for Chess
-        chess_parser = subparsers.add_parser("chess", help="Play the classic game of chess.")
+        chess_parser = subparsers.add_parser("chess", help="Play the classic game of chess.\n")
         chess_parser.add_argument("--mode",
                                   type=str,
                                   default="Multiplayer",
                                   choices=["Singleplayer", "Multiplayer"],
-                                  help="Two players can play on the same terminal (Multiplayer) or you can play alone against an AI (Singleplayer).")
+                                  help="Two players can play on the same terminal (Multiplayer) or you can play alone against an AI (Singleplayer).\n")
 
         return parser.parse_args()
 
